75d0a8ec6731220fe2ddab8796c771185c976fe0
Abhishek Mittal <abhi@Shubhams-MacBook-Pro.local>
2023-06-17 23:26:57 +0530

    dashboard base setup

--- src/pages/dashboard/Dashboard.style.tsx
+++ src/pages/dashboard/Dashboard.style.tsx
@@ -1,1 +1,7 @@
-export {};
+import styled from "styled-components";
+
+export const DashboardContainer = styled.div`
+  background-color: red;
+  width: 100%;
+  height: 100px;
+`;
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,20 +1,8 @@
 import React from "react";
+import { DashboardContainer } from "./Dashboard.style";
 
 function Dashboard() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Main Dashboard
-        </a>
-      </header>
-    </div>
-  );
+  return <DashboardContainer></DashboardContainer>;
 }
 
 export default Dashboard;
