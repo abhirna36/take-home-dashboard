9cbc2ed4d01925a7590ff963715829abb977ceb9
Abhishek Mittal <abhi@Shubhams-MacBook-Pro.local>
2023-06-18 00:44:51 +0530

    assets included

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { DashboardContainer } from "./Dashboard.style";
+import Header from "../../components/molecules/header";
 
 function Dashboard() {
-  return <DashboardContainer></DashboardContainer>;
+  return (
+    <DashboardContainer>
+      <Header />
+    </DashboardContainer>
+  );
 }
 
 export default Dashboard;
